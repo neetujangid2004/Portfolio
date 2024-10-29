7e04d618f466331d7a4fa42edad4b47dc25a043c
neetujangid2004 <neetujangid166@gmail.com>
2024-10-29 02:08:42 -0700

    modify new script

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+// contact form **********
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzwU7qzU6I1-xN8nHwmWqB8AYcRFtkNhOanOmspeNunrdKDw4YmOJZN4YNpn9jV1fVU/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully";
+        setTimeout(function(){
+            msg.innerHTML = "";
+        },5000)
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
